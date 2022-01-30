68a2505f9df103a491bca69963e5ce7442f0c6fc
Topias Mariapori <topias@mariapori.fi>
2022-01-30 21:01:31 +0200

    Eka release versio

--- app/src/main/java/fi/mariapori/rs3stats/MainActivity.java
+++ app/src/main/java/fi/mariapori/rs3stats/MainActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button asetukset = (Button) findViewById(R.id.btnAsetukset);
+        Button asetukset = (Button)findViewById(R.id.btnAsetukset);
         asetukset.setOnClickListener(v -> {
             Intent switchActivityIntent = new Intent(this, SettingsActivity.class);
             startActivity(switchActivityIntent);
@@ -35,34 +39,101 @@ public class MainActivity extends AppCompatActivity {
             statsList = (ListView) findViewById(R.id.statsList);
             StatsAdapter adapter = new StatsAdapter(this,R.layout.stat,stats);
             statsList.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
-            Thread newThread = new Thread(() -> {
-                try {
-                    URL url = new URL("https://secure.runescape.com/m=hiscore/index_lite.ws?player=" + accountName);
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setRequestMethod("GET");
-                    BufferedReader br = null;
-                    if (con.getResponseCode() == 200) {
-                        br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                        String strCurrentLine;
-                        int counter = 0;
-                        while ((strCurrentLine = br.readLine()) != null) {
-                            try {
-                                String lvl = strCurrentLine.split(",")[1];
-                                StatsObject uus = new StatsObject(RunescapeStats.values()[counter], Integer.getInteger(lvl));
-                                stats.add(uus);
-                                counter++;
-                            } catch (Exception ex1) {
+
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url ="https://secure.runescape.com/m=hiscore/index_lite.ws?player=" + accountName;
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try{
+                                BufferedReader br = new BufferedReader(new StringReader(response));
+                                String strCurrentLine;
+                                int counter = 0;
+                                while ((strCurrentLine = br.readLine()) != null) {
+                                    try {
+                                        int lvl = Integer.parseInt(strCurrentLine.split(",")[1]);
+                                        StatsObject uus = new StatsObject(RunescapeStats.values()[counter],lvl);
+                                        counter++;
+                                        stats.add(uus);
+                                        adapter.notifyDataSetChanged();
+                                    }catch(Exception ex2) {
+
+                                    }
+                                }
+                            }catch (Exception ex1){
+
                             }
                         }
-                    }
-                }catch (Exception ex2){
-                    Toast.makeText(this,"Can't update", Toast.LENGTH_LONG);
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
                 }
             });
-            newThread.start();
 
-    }
+            queue.add(stringRequest);
+
+
+        }
 
 }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_main);
+        Button asetukset = (Button)findViewById(R.id.btnAsetukset);
+        asetukset.setOnClickListener(v -> {
+            Intent switchActivityIntent = new Intent(this, SettingsActivity.class);
+            startActivity(switchActivityIntent);
+        });
+        String accountName = getSharedPreferences("Asetukset",MODE_PRIVATE).getString("accountName", "");
+        if(accountName == null || accountName == ""){
+            Intent switchActivityIntent = new Intent(this, SettingsActivity.class);
+            startActivity(switchActivityIntent);
+        }else{
+            statsList = (ListView) findViewById(R.id.statsList);
+            StatsAdapter adapter = new StatsAdapter(this,R.layout.stat,stats);
+            statsList.setAdapter(adapter);
+
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url ="https://secure.runescape.com/m=hiscore/index_lite.ws?player=" + accountName;
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try{
+                                BufferedReader br = new BufferedReader(new StringReader(response));
+                                String strCurrentLine;
+                                int counter = 0;
+                                while ((strCurrentLine = br.readLine()) != null) {
+                                    try {
+                                        int lvl = Integer.parseInt(strCurrentLine.split(",")[1]);
+                                        StatsObject uus = new StatsObject(RunescapeStats.values()[counter],lvl);
+                                        counter++;
+                                        stats.add(uus);
+                                        adapter.notifyDataSetChanged();
+                                    }catch(Exception ex2) {
+
+                                    }
+                                }
+                            }catch (Exception ex1){
+
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            });
+
+            queue.add(stringRequest);
+
+
+        }
+    }
 }
